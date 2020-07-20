fddce3b1c9e41067143598d75217804bd5c0b11a
Vinoth <you@example.com>
2020-07-20 14:11:35 +0530

    remove extra height form main content

--- src/pages/Maintennace/index.js
+++ src/pages/Maintennace/index.js
@@ -1,17 +1,17 @@
 import React, {Fragment} from "react";
 import { Table, TitleContainer, FilterContainer } from "../../components";
 import {Images} from "../../assets/images";
-
+import "./Maintennace.scss"
 const Maintennace = () => {
   return (
-    <Fragment>
+    <div className="MaintennacePage">
       <TitleContainer
         name="Maintennace"
         img={Images.Maintennace}
       />
       <FilterContainer name="Maintennace" />
       <Table name="Maintennace" />
-    </Fragment>
+    </div>
   );
 };
 
